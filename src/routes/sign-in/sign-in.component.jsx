d239445de16384100672034a321e7b2e3af028f5
Renner Renner <rennerclouds@gmail.com>
2023-01-06 21:01:38 +0100

    created Authentication page component alongside sign in form

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,20 +1,13 @@
-import { signInWithGooglePopup } from "../../utils/firebase/firebase.utils";
-import { createUserDocumentFromAuth } from "../../utils/firebase/firebase.utils";
-import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
 
+import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
+import SignInForm from "../../components/sign-in-form/sign-in-form.component";
+import './authentication.styles.scss'
 
 const SignIn = () => {
-    const logGoogleUser = async () => {
-        const {user} = await signInWithGooglePopup();
-        const userDocRef = await createUserDocumentFromAuth(user);
-        // console.log({user});
-    }
+   
     return(
-        <div>
-            <h1>Sign in</h1>
-            <button onClick={logGoogleUser}>
-            Sign in With Google
-            </button>
+        <div className="authentication-container">
+            <SignInForm />
             <SignUpForm />
         </div>
     );
