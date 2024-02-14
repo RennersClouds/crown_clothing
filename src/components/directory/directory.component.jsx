4dbd1b189bc42f5532171981012817505ef2476f
RennerClouds <rennerclouds@gmail.com>
2024-02-14 15:40:58 +0100

    Updated the test case

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,23 +1,23 @@
 
-// import CategoryItem from "../category-item/category-item.component";
-// import './directory.styles.scss'
+import CategoryItem from "../category-item/category-item.component";
+import './directory.styles.scss'
 
-// const Directory = ({categories}) =>{
+const Directory = ({categories}) =>{
 
-//     return (
-//         <div>
-//             <div className="directory-container">
-//                 {categories.map((category) => (
+    return (
+        <div>
+            <div className="directory-container">
+                {categories.map((category) => (
                 
-//                 <CategoryItem key={category.id} category={category} />
+                <CategoryItem key={category.id} category={category} />
             
-//                 ))}
+                ))}
                 
-//             </div> 
+            </div> 
     
-//          </div>
+         </div>
 
-//     )
-// }
+    )
+}
 
-// export default Directory
+export default Directory
